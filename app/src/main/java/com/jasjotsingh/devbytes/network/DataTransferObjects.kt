0329c44a65f53324ce1e557cc293b06a13e873e2
Jasjot Singh <jasjot784@gmail.com>
2020-08-06 21:10:58 +0530

    Starter code commit

---
+++ app/src/main/java/com/jasjotsingh/devbytes/network/DataTransferObjects.kt
@@ -0,0 +1,33 @@
+package com.jasjotsingh.devbytes.network
+
+import com.jasjotsingh.devbytes.domain.DevByteVideo
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class NetworkVideoContainer(val videos: List<NetworkVideo>)
+
+/**
+ * Videos represent a devbyte that can be played.
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkVideo(
+    val title: String,
+    val description: String,
+    val url: String,
+    val updated: String,
+    val thumbnail: String,
+    val closedCaptions: String?)
+
+/**
+ * Convert Network results to database objects
+ */
+fun NetworkVideoContainer.asDomainModel(): List<DevByteVideo> {
+    return videos.map {
+        DevByteVideo(
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail)
+    }
+}
--- app/src/main/java/com/jasjotsingh/devbytes/ui/MainActivity.kt
+++ app/src/main/java/com/jasjotsingh/devbytes/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.jasjotsingh.devbytes
+package com.jasjotsingh.devbytes.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jasjotsingh.devbytes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_dev_byte_viewer)
     }
 }
